df726699eec6927fb5ca48dc9ce46195f9df100a
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-06-04 22:16:32 -0500

    Add files via upload

---
+++ ListasCircularesTencionDeProcesos/ListasCircularesTencionDeProcesos/Estructura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesTencionDeProcesos
+{
+    class Estructura
+    {
+        Proceso inicio, ultimo = null;
+        
+
+        public void encolar(Proceso nuevo)
+        {
+            if(inicio == null)
+            {
+                inicio = nuevo;
+                inicio.Siguiente = inicio;
+                ultimo = nuevo;
+            }
+            else
+            {
+                ultimo.Siguiente = nuevo;
+                nuevo.Siguiente = inicio;
+                ultimo = nuevo;
+            }
+        }
+    }
+}
---
+++ ListasCircularesTencionDeProcesos/ListasCircularesTencionDeProcesos/Proceso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasCircularesTencionDeProcesos
+{
+    class Proceso
+    {
+        public int _ciclos;
+        private Proceso _siguiente;
+        Random rnd = new Random();
+
+        public Proceso()
+        {
+            _ciclos = rnd.Next(4, 15);
+        }
+
+        public override string ToString()
+        {
+            return "Ciclos: " + _ciclos;
+        }
+
+        public Proceso Siguiente
+        {
+            get { return _siguiente; }
+            set { _siguiente = value; }
+        }
+    }
+}
